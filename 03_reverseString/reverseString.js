c17c75e8713d005de6f9b21e92422c4dea330a69
Adam Ford <ford.adm@gmail.com>
2024-06-15 15:40:59 -0500

    created this repo for personal use, and started at exercise 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,15 @@
 const reverseString = function() {
+    let testArray = [... new Set(test)];
+    let toRemove = [...remove];
+
+    
+    for (let i = 0; i < toRemove.length; i++) {
+        let index = toRemove[i];
+        if (testArray[i] === testArray.indexOf(index));
+            testArray.splice(testArray.indexOf(index), 1);
+    }
+
+    return testArray;
 
 };
 
