b0a359eb3f104fabadf67cba87fa538403d3cf61
Adam Ford <ford.adm@gmail.com>
2024-06-15 16:17:59 -0500

    complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(test, ...remove) {
+    let testArray = [... new Set(test)];
+    let toRemove = [...remove];
+
+    for (let i = 0; i < toRemove.length; i++) {
+        let index = toRemove[i];
+            if (testArray.includes(index)) {
+            testArray.splice(testArray.indexOf(index), 1);
+            }
+    }
+
+    return testArray;
 };
 
 // Do not edit below this line
