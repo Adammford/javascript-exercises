f21b3e59aabbad4bbd6217ad7ac1b7231f3e6288
Adam Ford <ford.adm@gmail.com>
2024-06-15 17:27:37 -0500

    completes exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let finalSum;
+    if (num1 < 0 || num2 < 0) return 'ERROR';
+    if (Number.isInteger(num1) && Number.isInteger(num2)) {
+        if (num1 < num2) {
+            for (i = 0; i < num2; i++) {
+                finalSum = (num2 * (num1 + num2)) / 2
+            }
+        } else {
+            for (i = 0; i < num1; i++) {
+                finalSum = (num1 * (num2 + num1)) / 2
+            }
+        }
+    } else {
+        return finalSum = 'ERROR';
+    }
+    return finalSum;
 
 };
 
