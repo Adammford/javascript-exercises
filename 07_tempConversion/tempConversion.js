ae3109cff5285fca12c9db075368bea1398138ff
Adam Ford <ford.adm@gmail.com>
2024-06-15 19:04:22 -0500

    Completed exercise 7 and all that is needed for TOP lesson

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let fahrenheit = ((fahr - 32) * (5 / 9));
+  if (Number.isInteger(fahrenheit)) {
+    return fahrenheit
+  } else {
+    return Number(fahrenheit.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let celsius = ((9/5) * cels) + 32;
+  if (Number.isInteger(celsius)) {
+    return celsius
+  } else {
+    return Number(celsius.toFixed(1));
+  }
 };
+
 
 // Do not edit below this line
 module.exports = {
