fa143b5564201f3ba59f71f058e282fe33b00518
Adam Ford <ford.adm@gmail.com>
2024-09-05 19:23:42 -0500

    completed exercises 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let mySum = 0;
+  array.forEach( num => {
+    mySum += num;
+})
+  return mySum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let newNum = 1;
+  array.forEach( num => {
+    newNum *= num;
+  })
+  return newNum;
 };
 
-const power = function() {
-	
+const power = function(num1, power) {
+  let newNum = 1;
+  for (let i = 0; i < power; i++) {
+    newNum *= num1; 
+  }
+  return newNum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1;
+  let factor = 0;
+  for (let i = 0; i < num; i++) {
+    factorial *= (num - factor);
+    factor++; 
+  }
+  return factorial;
+
 };
 
 // Do not edit below this line
