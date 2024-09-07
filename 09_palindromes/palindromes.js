497bce8d377f93cb18cb66746fad5a56246c66d8
Adam Ford <ford.adm@gmail.com>
2024-09-07 00:30:30 -0500

    completes exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,33 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+
+    let lowerPhrase = phrase.toLowerCase();
+    let newArray = lowerPhrase.split('').filter(str => str.match(/[^_\W]+/g));
+    let reversedArray = [];
+
+    for (let i = newArray.length - 1; i >= 0; i--) {
+        reversedArray.push(newArray[i]);
+    }
+
+    let isTrue = 1;
+
+    for (let i = 0; i < newArray.length; i++) {
+        if (newArray[i] !== reversedArray[i]) {
+            isTrue --;
+        }
+    }
+
+    if (isTrue === 1) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+
+
+
