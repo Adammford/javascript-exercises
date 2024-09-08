9ff317caf0424376d38fe97333e7e78b379ba5a2
Adam Ford <ford.adm@gmail.com>
2024-09-07 20:35:19 -0500

    completes ex 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,26 +1,14 @@
 const palindromes = function (phrase) {
 
-    let lowerPhrase = phrase.toLowerCase();
-    let newArray = lowerPhrase.split('').filter(str => str.match(/[^_\W]+/g));
-    let reversedArray = [];
+    let cleanedString = phrase
+        .toLowerCase()
+        .split('')
+        .filter(str => str.match(/[^_\W]+/g))
+        .join('');
+    
+    let reversedString = cleanedString.split('').reverse().join('');
 
-    for (let i = newArray.length - 1; i >= 0; i--) {
-        reversedArray.push(newArray[i]);
-    }
-
-    let isTrue = 1;
-
-    for (let i = 0; i < newArray.length; i++) {
-        if (newArray[i] !== reversedArray[i]) {
-            isTrue --;
-        }
-    }
-
-    if (isTrue === 1) {
-        return true;
-    } else {
-        return false;
-    }
+    return reversedString === cleanedString;
 
 };
 
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
 
+
+const fibonacci = function(num) {
+    let fibArray = [0, 1];
+    if (num < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 0; i < num; i++) {
+        fibArray.push(fibArray[i] + fibArray[i + 1]);
+        }
+        return fibArray[num];
+    }
 };
 
 // Do not edit below this line
