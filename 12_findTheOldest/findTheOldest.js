7159bad897eefbeecb45274bc5a49ea8c7a94184
Adam Ford <ford.adm@gmail.com>
2024-09-08 01:19:17 -0500

    starts ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    
+    let birth = people.map((age) => age.yearOfBirth);
+    let death = people.map((age) => age.yearOfDeath);
+    let age = [];
+    let oldest = 0;
+    for (let i = 0; i < people.length; i++) {
+        age.push(death[i] - birth[i]);
+    }
+
+    for (let i = 0; i < age.length; i++) {
+        if (age[i] > oldest) {
+            oldest = age[i];
+        }
+    }
+    return people[age.indexOf(oldest)];
 
 };
 
